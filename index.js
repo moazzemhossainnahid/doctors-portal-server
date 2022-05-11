781982f4b60036eaa7098d8b727fb557b4d13f63
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-05-11 17:44:29 +0600

    get services

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,6 +9,33 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
+
+
+const uri = `mongodb+srv://doctors_admin:zY9QBegeqE3r8fYx@cluster0.ytub2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async() => {
+    try{
+        await client.connect();
+        const serviceCollection = client.db("DoctorsPortal").collection("Service");
+        console.log('db connected');
+
+
+        // get Service
+        app.get('/services', async(req, res)=>{
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+
+    }finally{
+
+    }
+}
+
+run().catch(console.dir);
 app.get('/', (req, res) => {
     res.send('Doctors Portal Server is Running')
 })
